486cab3d326ae405872821f26e6bfa0b377cac0e
BrittanyR <brittany_rutherford@outlook.com>
2017-05-14 14:57:47 -0700

    another commit

--- app/ts/newpost.ts
+++ app/ts/newpost.ts
@@ -1,23 +1,35 @@
 import './../css/master.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
-    let btnSubmit = document.getElementById('submitpost')
+    let btnSubmit = document.getElementById('submitpost') as HTMLInputElement
 
     btnSubmit.addEventListener('click', () => {
-        let postTitle = document.getElementById('postTitle').value
-        let postDate = document.getElementById('postDate').value
-        let author = document.getElementById('author').value
-        let commentsCount = document.getElementById('commentsCount').value
-        let postContent = document.getElementById('postContent').value
+        let postTitleInput = document.getElementById('postTitle') as HTMLInputElement | null
+        let postTitle = postTitleInput?  postTitleInput.value : 'Default Title'
+
+        let postDateInput = document.getElementById('postDate') as HTMLInputElement | null
+        let postDate =  postDateInput? postDateInput.value : 'Default Title'
+
+
         
+        let authorInput = document.getElementById('author') as HTMLInputElement | null
+        let author = authorInput?  authorInput.value : 'Default Title'
+        
+           
+        let commentsCountInput = document.getElementById('commentsCount') as HTMLInputElement | null
+        let commentsCount =  commentsCountInput? commentsCountInput.value : 'Default Title'
+      
+        let postContentInput = document.getElementById('postContent') as HTMLInputElement | null
+        let postContent = postContentInput ? postContentInput.value : 'Default Title'
+
         fetch('http://localhost:3000/posts', {
-            method: 'POST', 
+            method: 'POST',
             body: {
-                "postTitle": postTitle , 
-                "postDate": postDate, 
-                "author": author , 
-                "commentsCount" :commentsCount, 
-                "postContent" : postContent
+                "postTitle": postTitle,
+                "postDate": postDate,
+                "author": author,
+                "commentsCount": commentsCount,
+                "postContent": postContent
             }
         }).then((res) => res.json())
             .then((data) => console.log(data))
